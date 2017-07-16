55226054111a82b27e2f87181b30edb207ef3251
SasaRA <RA@souljah.com>
2017-07-16 05:05:55 -0700

    clients/avivagemassage initial commit

---
+++ avivagemassage/form.php
@@ -0,0 +1,26 @@
+<h3>Contact Us:</h3>
+<form method="post" action="contact.php">
+<p class="name"> 
+	<label for="name">Name</label> 
+	<input type="text" maxlength="50" name="name" id="name" /> 
+</p>
+
+<p class="email"> 
+	<label for="email">E-mail</label> 
+	<input type="email" maxlength="50" name="email" id="email" /> 
+</p>
+
+<p class="tel"> 
+	<label for="tel">Phone</label>
+	<input type="tel" maxlength="16" name="tel" id="tel" />  
+</p>
+
+<p class="text">
+	<label for="text">Message</label>
+	<textarea maxlength="1500" name="text"></textarea> 
+</p> 
+
+<p class="submit"> 
+	<input type="submit" value="Submit" /> 
+</p>
+</form>
---
+++ avivagemassage/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Avivage Massage</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="RNE14gcfqBJzkRr1U45Zb5mqAOrdmpsjvCYpuhr5jI4" />
+	<link rel="stylesheet" href="assets/avivage.css">
+	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+	<link rel="shortcut icon" href="">
+	<script type="text/javascript" src="assets/smooth-scroll.min.js"></script>
+	<script type="text/javascript" src="assets/wsLaunch.js"></script>
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	
+</head>
+<body>
+
+
+
+<div id="container">
+	<header>
+		<div id="lo"><img id="logo" src="assets/avivage_hero.png"></div>	
+		<nav><ul>
+			<li><a data-scroll href="http://avivagemassage.com/#about">About</a></li>
+			<li><a data-scroll href="http://avivagemassage.com/#modalities">Modalities</a></li>
+			<li><a href="javascript:launchWS('http://clients.mindbodyonline.com/ws.asp?studioid=24421&stype=-9&sTG=1&sView=day');">Schedule Online</a></li>
+			<li><a data-scroll href="http://avivagemassage.com/#contact">Contact</a></li>
+			<li><a href="javascript:launchWS('http://clients.mindbodyonline.com/classic/ws?studioid=24421&stype=42');">Gift Cards</a></li>
+		</ul></nav>
+	</header>
+<section>
+<article>
+	<div class="content">
+		<h1>
